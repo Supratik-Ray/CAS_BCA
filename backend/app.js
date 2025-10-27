35ade82124ca00728cbcdaa2e762aed9c76366f2
Supratik Ray <raySupratik8@gmail.com>
2025-10-27 13:49:30 +0530

    add documentation

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,9 @@ import dotenv from "dotenv";
 import express from "express";
 import fileUpload from "express-fileupload";
 import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./swagger-output.json" with {type:"json"}
+
 import connectDB from "./db/connect.js";
 import authRouter from "./routes/authRoutes.js";
 import projectRouter from "./routes/projectRoutes.js";
@@ -18,8 +21,10 @@ app.use(express.json());
 app.use(fileUpload({ useTempFiles: true }));
 
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.send("<h1>CAS API</h1><a href='/api-docs'>api docs</a>");
 });
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/projects", authenticationMiddleware, projectRouter);
